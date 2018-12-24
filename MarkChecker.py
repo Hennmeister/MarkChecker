de32d8a7245f4a7fe43ecbe6b86264b644a7130d
Henning Lindig <henning.lindig@rogers.com>
2018-12-24 13:25:01 -0500

    Now saves current marks if they are different than previous version

--- MarkChecker.py
+++ MarkChecker.py
@@ -3,26 +3,57 @@ import re
 url = 'https://ta.yrdsb.ca/yrdsb/index.php'
 values = {'username': '',
           'password': ''}
-
-r = requests.post(url, data=values)
-response = r.content;
-
 count = 0
 search_str = "mark "
 courseCode = re.compile("([A-Z][A-Z][A-Z][0-9][A-Z][0-9]-[0-9][0-9])")
+marks = {}
+newMarks = {}
+course = ""
+mark = ""
+inp1 = ""
+inp2 = ""
+#r = requests.post(url, data=values)
+#response = r.content;
 
+#get curr input DELETE
+with open('responseData', 'r') as d : 
+	response = d.read()
 
-for i in range(len(r.content)) : 
-	if courseCode.match(r.content[i : i + 9 ]) :
-		print r.content[i : i + 9]
-		if r.content.find("Please see teacher for current status regarding achievement in the course", i) > -1 :
-			if r.content.find(search_str, i) < r.content.find("Please see teacher for current status regarding achievement in the course", i) :
-				print r.content[r.content.find(search_str, i) : r.content.find(search_str, i)+13]
+#GOING TO HAVE TO CHANGE THIS TO READ LAST ENTRY 
+with open('History', 'r') as m:
+	inp1 = m.readline().rstrip()
+	while inp1 or inp2:
+		inp2 = m.readline().rstrip()
+		d = {inp1: inp2}
+		marks.update(d)
+		inp1 = m.readline().rstrip()
+if '' in marks :
+	del marks['']
+
+for i in range(len(response)) : 
+	if courseCode.match(response[i : i + 9 ]) :
+		course = response[i : i + 9]
+		if response.find("Please see teacher for current status regarding achievement in the course", i) > -1 :
+			if response.find(search_str, i) < response.find("Please see teacher for current status regarding achievement in the course", i) :
+				mark = response[response.find(search_str, i) + 10 : response.find(search_str, i)+13]
 			else :	
-				print "Mark unavailable"
+				mark = 'Mark unavailable'
 		else : 
-				print r.content[r.content.find(search_str, i) : r.content.find(search_str, i)+13]
+			mark = response[response.find(search_str, i) + 8 : response.find(search_str, i)+13]
+	add = {course: mark}
+	newMarks.update(add)
+del newMarks['']
 
+if marks != newMarks :
+	with open('History', 'a') as a:
+		a.write("\n")
+		for k, v in newMarks.iteritems() :
+			a.write(k)
+			a.write("\n")
+			a.write(v)
+			a.write("\n")
+
+#ADD DATE
 #checking for a change
 #saving to file
 #reading from file
