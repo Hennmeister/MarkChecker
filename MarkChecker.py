c6ccc5e1de9fdbf6d30faedf9e8e12ec6b6f4e2a
Henning Lindig <henning.lindig@rogers.com>
2019-01-06 23:11:25 -0500

    Refactored code and now used env vars. TK window does not display course names now.

--- MarkChecker.py
+++ MarkChecker.py
@@ -1,30 +1,24 @@
 import requests
 import re
+import os
 from datetime import date
 from Tkinter import *
+
+date = str(date.today())
+
 url = 'https://ta.yrdsb.ca/yrdsb/index.php'
-values = {'username': '',
-          'password': ''}
-count = 0
-search_str = "mark "
-courseCode = re.compile("([A-Z][A-Z][A-Z][0-9][A-Z][0-9]-[0-9][0-9])")
-marks = {}
-newMarks = {}
-course = ""
-mark = ""
-inp1 = ""
-inp2 = ""
+values = {'username': os.environ['TaUser'],
+          'password': os.environ['TaPass']}
 r = requests.post(url, data=values)
 response = r.content;
 
-#get curr input DELETE
-#with open('responseData', 'r') as d : 
-#	response = d.read()
 def FindUpdatedCourses():
 	lineCount = 0
 	diff = ""
+	newMarks = GetNewMarks()
+	recentMarks = GetRecentMarks()
 	for key in newMarks.keys() : 
-		if marks.get(key) != newMarks.get(key) :
+		if recentMarks.get(key) != newMarks.get(key) :
 			diff += key + "\n"
 	return diff
 
@@ -38,40 +32,58 @@ def UpdateWindow():
 	T.insert(END, text)
 	root.mainloop()
 
-with open('History', 'r') as f:
-	f.seek(-64, 2)
-	inp1 = f.readline().rstrip()
-	while inp1 or inp2:
-		inp2 = f.readline().rstrip()
-		d = {inp1: inp2}
-		marks.update(d)
+def GetRecentMarks():
+	marks = {}
+	inp1 = ""
+	inp2 = ""
+	with open('/Users/henningl/MarkChecker/History', 'r') as f:
+		f.seek(-64, 2)
 		inp1 = f.readline().rstrip()
-if '' in marks :
-	del marks['']
+		while inp1 or inp2:
+			inp2 = f.readline().rstrip()
+			d = {inp1: inp2}
+			marks.update(d)
+			inp1 = f.readline().rstrip()
+	if '' in marks :
+		del marks['']
+	return marks
 
-for i in range(len(response)) : 
-	if courseCode.match(response[i : i + 9 ]) :
-		course = response[i : i + 9]
-		if response.find("Please see teacher for current status regarding achievement in the course", i) > -1 :
-			if response.find(search_str, i) < response.find("Please see teacher for current status regarding achievement in the course", i) :
-				mark = response[response.find(search_str, i) + 10 : response.find(search_str, i)+13]
-			else :	
-				mark = 'Mark unavailable'
-		else : 
-			mark = response[response.find(search_str, i) + 8 : response.find(search_str, i)+13]
-	add = {course: mark}
-	newMarks.update(add)
-del newMarks['']
+def GetNewMarks():
+	newMarks = {}
+	search_str = "mark "
+	course = ""
+	mark = 0
+	courseCode = re.compile("([A-Z][A-Z][A-Z][0-9][A-Z][0-9]-[0-9][0-9])")
 
-if marks != newMarks :
-	with open('History', 'a') as a:
-		a.write("\n")
-		date = str(date.today())
-		a.write(date)
-		a.write("\n")
-		for k, v in newMarks.iteritems() :
-			a.write(k)
+	for i in range(len(response)) : 
+		if courseCode.match(response[i : i + 9 ]) :
+			course = response[i : i + 9]
+			if response.find("Please see teacher for current status regarding achievement in the course", i) > -1 :
+				if response.find(search_str, i) < response.find("Please see teacher for current status regarding achievement in the course", i) :
+					mark = response[response.find(search_str, i) + 10 : response.find(search_str, i)+13]
+				else :	
+					mark = 'Mark unavailable'
+			else : 
+				mark = response[response.find(search_str, i) + 8 : response.find(search_str, i)+13]
+		add = {course: mark}
+		newMarks.update(add)
+	del newMarks['']
+	return newMarks
+
+def main():
+	newMarks = GetNewMarks()
+	if GetRecentMarks() != newMarks :
+		with open('/Users/henningl/MarkChecker/History', 'a') as a:
 			a.write("\n")
-			a.write(v)
+			a.write(date)
 			a.write("\n")
-	UpdateWindow()
+			for k, v in newMarks.iteritems() :
+				a.write(k)
+				a.write("\n")
+				a.write(v)
+				a.write("\n")
+		UpdateWindow()
+
+
+if __name__ == '__main__':
+	main()
